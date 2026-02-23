b8ddfcb9fe46373360a28d95891fc90c425afc0c
XavZol <alansolis707@gmail.com>
2026-02-22 21:34:45 -0600

    Comienzo de Arreglos.

---
+++ Vectores.java
@@ -0,0 +1,17 @@
+
+
+
+public class Vectores {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[3];
+
+        numeros[0] = 7;
+        numeros[1] = 14;
+        numeros[2] = 18;
+        
+        for(int i=0; i<3; i++){
+            System.out.println(numeros[i]);
+        }
+    }
+}
