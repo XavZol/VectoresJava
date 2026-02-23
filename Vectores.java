091784cb33512e9e1aa16c0346d47fe0af538041
XavZol <alansolis707@gmail.com>
2026-02-23 01:08:10 -0600

    Desplazar una posición en un arreglo.

--- Vectores.java
+++ Vectores.java
@@ -2,36 +2,31 @@
 import java.util.Scanner;
 
 
-/*Diseñar una aplicación que declare una tabla de 10 elementos enteros. Leer mediante el teclado 8números.
-Depués se debe pedir un número y una posición, insertarlo en la posición indicada, desplazando los que estén detrás. */
+/*Diseñar una aplicación que declare una tabla de 10 elementos enteros y la desplace una posición hacia abajo: el primero pasa a ser el segundo, el segundo pasa 
+a ser el tercero y así sucesivamente. EL último pasa a ser el primero. */
 public class Vectores {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int arreglo[] = new int[10]; // Creamos el arreglo de 10 elementos enteros
-        int numero, posicion;
-
-        System.out.println("Llenamos el arreglo");
-        for(int i=0; i<8; i++) {
-            System.out.print((i)+". Digite un número: ");
+        int arreglo[] = new int[10];
+        int ultimo;
+        System.out.println("Llenar el arreglo");
+        for(int i=0; i<10; i++) {
+            System.out.print(i+". Digite un número: ");
             arreglo[i] = entrada.nextInt();
         }
 
-        System.out.print("\nDigite un número: ");
-        numero = entrada.nextInt();
-        System.out.print("\nDigite una posición: ");
-        posicion = entrada.nextInt();
+        ultimo = arreglo[9]; //Guardamos el ultimo elemento
 
-        // Correr una posición los elementos detrás de la posición que el usuario quiere
-        for(int i=7; i>=posicion; i--) { // 1-2-3-4-5-6
+        for(int i=8; i>=0; i--) { // Avanzar una posicion abajo en el arreglo 
             arreglo[i+1] = arreglo[i];
         }
 
-        arreglo[posicion] = numero; // insertamos el número en la posición requerida
+        arreglo[0] = ultimo; // Ponemos el ultimo elemento como el primero 
 
-        System.out.println("El nuevo arreglo es: ");
-        for(int i=0; i<9; i++) {
-            System.out.println("Posicion "+i+": "+arreglo[i]);
+        System.out.println("\nEl nuevo arreglo es: ");
+        for(int i=0; i<10; i++){
+            System.out.println(i+". Número: "+arreglo[i]);
         }
     }
 }
