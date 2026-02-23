5d17c68c252d81c48effde2d5741cd5ef6590ebf
XavZol <alansolis707@gmail.com>
2026-02-23 03:47:28 -0600

    Buscar un elemento en un arreglo ordenado.

--- Vectores.java
+++ Vectores.java
@@ -1,104 +1,59 @@
 
 import java.util.Scanner;
 
-/*Leer dos series de 10 enteros, que estarán ordenados crecientemente. Copiar (fusionar) las dos tablas en una tercera, de forma que sigan ordenados */
+/*Leer 10 enteros ordenados crecientemente. Leer N y buscarlo en la tabla. Se debe mostrar la posición en que se encuentra. Si 
+no está, indicarlo en mensaje. */
 public class Vectores {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int a[], b[], c[];
+        int arreglo[], numero;
         boolean creciente = true;
 
-        a = new int[10]; // 10 elementos para el arreglo a
-        b = new int[10]; //también para el arreglo b
-        c = new int[20]; //20 elementos para arreglo c
-
-        System.out.println("Digite el primer arreglo: ");
+        arreglo = new int[10]; // Creamos el arreglo con 10 posiciones
         do {
-            
-            // Pedimos el arreglo
+            // Pedimos el arreglo 
+            System.out.println("Rellene el arreglo: ");
             for(int i=0; i<10; i++) {
-                System.out.print((i+1)+". Digite un número: ");
-                a[i] = entrada.nextInt();
+                System.out.print(i+". Digite un número: ");
+                arreglo[i] = entrada.nextInt();
             }
-
-            // Comprobamos si el arreglo esta ordenado
+            // Verifica si el arreglo esta ordenado
             for(int i=0; i<9; i++) {
-                if(a[i]< a[i+1]) { // Creciente: 1-2-3-4..
+                if(arreglo[i] < arreglo[i+1]) { // Creciente: 1-2-3
                     creciente = true;
                 }
-                if(a[i] > a[i+1]) { // Decreciente: 3-2-1
-                    creciente= false;
+                if(arreglo[i] > arreglo[i+1]) { // Decreciente: 3-2-1
+                    creciente = false;
                     break;
                 }
             }
 
             if(creciente == false) {
-                System.out.println("\nEl arreglo está desordenado, vuelve a digitar: ");
+                System.out.println("\nEl arreglo esta desordenado, digite nuevamente: \n");
             }
-        } while(creciente == false);
-        
-        // Segundo arreglo
-        System.out.println("Digite el segundo arreglo: ");
-        do {
-            
-            // Pedimos el arreglo
-            for(int i=0; i<10; i++) {
-                System.out.print((i+1)+". Digite un número: ");
-                b[i] = entrada.nextInt();
-            }
+        } while(creciente == false); 
+    
+        // Pedimos el numero a buscar 
+        System.out.print("\nDigite el número a buscar en el arreglo: ");
+        numero = entrada.nextInt();
 
-            // Comprobamos si el arreglo esta ordenado
-            for(int i=0; i<9; i++) {
-                if(b[i]< b[i+1]) { // Creciente: 1-2-3-4..
-                    creciente = true;
-                }
-                if(b[i] > b[i+1]) { // Decreciente: 3-2-1
-                    creciente= false;
-                    break;
-                }
-            }
-
-            if(creciente == false) {
-                System.out.println("\nEl arreglo está desordenado, vuelve a digitar: ");
-            }
-        } while(creciente == false);
-
-        int i=0; // iterador i sera para el arreglo a 
-        int j=0; // iterador i sera para el arreglo b
-        int k=0; // iterador i sera para el arreglo c
-
-        while(i<10 && j<10) {
-            if(a[i] < b[j]) { // Si el elemento de a es menor de b
-                c[k] = a[i]; //Copiamos el elemento de a 
-                i++; // Avanzamos una posicion en a 
+        // Buscamos el numero en el arreglo
+        int i=0;
+        // 1-2-3-4-5...-10 
+        while(i<10 && arreglo[i]<numero) {
+            i++;
+        }
+        if(i == 10) { // Hemos recorrido todo el arreglo y no encontramos nada 
+            System.out.println("\nNúmero no encontrado");
+        }
+        else {
+            if(arreglo[i] == numero) {
+                System.out.println("\nNúmero encontrado en la posición: "+i);
             }
             else {
-                c[k] = b[j]; // copiamos el elemento de b
-                j++; // Avanzamos una posicion en b
-            }
-            k++; // Avanzamos una posicion en c
-        }
-        // Cuando salimos de while es porque un arreglo( a o b), sea copiamos completamente  
-        if(i == 10) { // Significa que ya copiamos el arreglo a, falta b
-            while(j<10) { // mientras el iterador sea menor a 10
-                c[k] = b[j]; // copiamos el elemento de b en c
-                j++; // Avanzamos una posicion en b 
-                k++; // Avanzamos una posicion en c
+                System.out.println("\nNúmero no encontrado");
             }
         }
-        else { // Significa que ya copiamos todo el arreglo b, falta el a
-            while(i<10) {
-                c[k] = a[i];
-                i++;
-                k++;
-            }
-        }
-        
-        System.out.println("\nEl arreglo C completo es: ");
-        for(k=0; k<20; k++) {
-            System.out.print(c[k]+" - ");
-        }
-        System.out.println("");
     }
 }
