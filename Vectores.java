e5a65e92d6b4bec7e9b01370c6e66de881730066
XavZol <alansolis707@gmail.com>
2026-02-23 03:02:26 -0600

    Mezclar los elementos de dos arreglos ordenadamente.

--- Vectores.java
+++ Vectores.java
@@ -1,55 +1,104 @@
 
 import java.util.Scanner;
 
-/*Leer 10 enteros en una tabla. Guardar en otra tabla los elementos pares de la primera, y a continuación los elementos impares. */
+/*Leer dos series de 10 enteros, que estarán ordenados crecientemente. Copiar (fusionar) las dos tablas en una tercera, de forma que sigan ordenados */
 public class Vectores {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int arreglo[] = new int[10]; //Creamos el arreglo para 10 elementos enteros
-        int conteo_pares=0, conteo_impares=0;
+        int a[], b[], c[];
+        boolean creciente = true;
 
-        System.out.println("Llenar el arreglo: ");
-        for(int i=0; i<10; i++) {
-            System.out.print((i+1)+". Digite un número: ");
-            arreglo[i] = entrada.nextInt();
+        a = new int[10]; // 10 elementos para el arreglo a
+        b = new int[10]; //también para el arreglo b
+        c = new int[20]; //20 elementos para arreglo c
 
-            if(arreglo[i] %2==0) { // Si el número es par 
-                conteo_pares++;
-            } else { // Si el número es impar 
-                conteo_impares++;
+        System.out.println("Digite el primer arreglo: ");
+        do {
+            
+            // Pedimos el arreglo
+            for(int i=0; i<10; i++) {
+                System.out.print((i+1)+". Digite un número: ");
+                a[i] = entrada.nextInt();
+            }
+
+            // Comprobamos si el arreglo esta ordenado
+            for(int i=0; i<9; i++) {
+                if(a[i]< a[i+1]) { // Creciente: 1-2-3-4..
+                    creciente = true;
+                }
+                if(a[i] > a[i+1]) { // Decreciente: 3-2-1
+                    creciente= false;
+                    break;
+                }
+            }
+
+            if(creciente == false) {
+                System.out.println("\nEl arreglo está desordenado, vuelve a digitar: ");
+            }
+        } while(creciente == false);
+        
+        // Segundo arreglo
+        System.out.println("Digite el segundo arreglo: ");
+        do {
+            
+            // Pedimos el arreglo
+            for(int i=0; i<10; i++) {
+                System.out.print((i+1)+". Digite un número: ");
+                b[i] = entrada.nextInt();
+            }
+
+            // Comprobamos si el arreglo esta ordenado
+            for(int i=0; i<9; i++) {
+                if(b[i]< b[i+1]) { // Creciente: 1-2-3-4..
+                    creciente = true;
+                }
+                if(b[i] > b[i+1]) { // Decreciente: 3-2-1
+                    creciente= false;
+                    break;
+                }
+            }
+
+            if(creciente == false) {
+                System.out.println("\nEl arreglo está desordenado, vuelve a digitar: ");
+            }
+        } while(creciente == false);
+
+        int i=0; // iterador i sera para el arreglo a 
+        int j=0; // iterador i sera para el arreglo b
+        int k=0; // iterador i sera para el arreglo c
+
+        while(i<10 && j<10) {
+            if(a[i] < b[j]) { // Si el elemento de a es menor de b
+                c[k] = a[i]; //Copiamos el elemento de a 
+                i++; // Avanzamos una posicion en a 
+            }
+            else {
+                c[k] = b[j]; // copiamos el elemento de b
+                j++; // Avanzamos una posicion en b
+            }
+            k++; // Avanzamos una posicion en c
+        }
+        // Cuando salimos de while es porque un arreglo( a o b), sea copiamos completamente  
+        if(i == 10) { // Significa que ya copiamos el arreglo a, falta b
+            while(j<10) { // mientras el iterador sea menor a 10
+                c[k] = b[j]; // copiamos el elemento de b en c
+                j++; // Avanzamos una posicion en b 
+                k++; // Avanzamos una posicion en c
             }
         }
-
-        // Crear los arreglos
-        int par[] = new int[conteo_pares]; // Creamos el arreglo para almacenar los números pares
-        int impar[] = new int[conteo_impares];// y ahora los números impares 
-
-        conteo_pares=0;
-        conteo_impares=0;
-
-        // Almacenamos los números pares en su arreglo y los impares también
-        for(int i=0; i<10; i++) {
-            if(arreglo[i] %2 == 0) { // Si el número es par
-                par[conteo_pares] = arreglo[i];
-                conteo_pares++;
-            } else { // Si el número es impar
-                impar[conteo_impares] = arreglo[i];
-                conteo_impares++;
+        else { // Significa que ya copiamos todo el arreglo b, falta el a
+            while(i<10) {
+                c[k] = a[i];
+                i++;
+                k++;
             }
         }
-
-        System.out.println("\nArreglo pares: ");
-        for(int i=0; i<conteo_pares; i++) {
-            System.out.print(par[i]+" - ");
+        
+        System.out.println("\nEl arreglo C completo es: ");
+        for(k=0; k<20; k++) {
+            System.out.print(c[k]+" - ");
         }
         System.out.println("");
-
-        System.out.println("\nArreglo impares: ");
-        for(int i=0; i<conteo_impares; i++) {
-            System.out.print(impar[i]+" - ");
-        }
-        System.out.println("");
-
     }
 }
