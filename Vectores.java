515a45e0d0bc2065d944434ec70ef488663cbe0b
XavZol <alansolis707@gmail.com>
2026-02-23 00:53:17 -0600

    Desplazar elementos de un arreglo hasta una posición.

--- Vectores.java
+++ Vectores.java
@@ -2,56 +2,36 @@
 import java.util.Scanner;
 
 
-/*Leer los datos correspondiente a dos tablas de 12 elementos númericos, y mezclarlos en una tercera de la forma: 3 de la tabla A, 3 de la 
-B, otros 3 de A, otros 3 de la B, etc. */
+/*Diseñar una aplicación que declare una tabla de 10 elementos enteros. Leer mediante el teclado 8números.
+Depués se debe pedir un número y una posición, insertarlo en la posición indicada, desplazando los que estén detrás. */
 public class Vectores {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int a[], b[], c[];
-        int i, j;
+        int arreglo[] = new int[10]; // Creamos el arreglo de 10 elementos enteros
+        int numero, posicion;
 
-        a = new int[12]; //Arreglo a[] va a tener 12 elementos
-        b = new int[12];
-        c = new int[24]; // Arreglo c[] va almacenar a arreglo a[] y arreglo b[]
-
-        //Pedimos el arreglo a[]
-        System.out.println("Digite el primer arreglo");
-        for( i=0; i<12; i++) {
-            System.out.print((i+1)+". Digite un número: ");
-            a[i] = entrada.nextInt();
-        }
-        
-        //Pedimos el arreglo b[]
-        System.out.println("Digite el segundo arreglo");
-        for( i=0; i<12; i++) {
-            System.out.print((i+1)+". Digite un número: ");
-            b[i] = entrada.nextInt();
-        }
-        // Mezclar los dos arreglos a[] y b[]
-        // El iterador i sera para los arreglos a[] y b[] y el j para el arreglo c[]
-        i=0;
-        j=0;
-
-        while(i<12) {
-            // Copiar los 3 elementos de a[]
-            for(int k=0; k<3; k++) {
-                c[j] = a[i+k];
-                j++;
-            }
-            //Ahora copiamos los 3 elementos de b[]
-            for(int k=0;k<3;k++) {
-                c[j] = b[i+k];
-                j++;
-            }
-
-            i+=3;
+        System.out.println("Llenamos el arreglo");
+        for(int i=0; i<8; i++) {
+            System.out.print((i)+". Digite un número: ");
+            arreglo[i] = entrada.nextInt();
         }
 
-        System.out.print("El arreglo resultante es: ");
-        for(i=0; i<24; i++) {
-            System.out.print(c[i]+" - ");
+        System.out.print("\nDigite un número: ");
+        numero = entrada.nextInt();
+        System.out.print("\nDigite una posición: ");
+        posicion = entrada.nextInt();
+
+        // Correr una posición los elementos detrás de la posición que el usuario quiere
+        for(int i=7; i>=posicion; i--) { // 1-2-3-4-5-6
+            arreglo[i+1] = arreglo[i];
         }
-        System.out.println();
+
+        arreglo[posicion] = numero; // insertamos el número en la posición requerida
+
+        System.out.println("El nuevo arreglo es: ");
+        for(int i=0; i<9; i++) {
+            System.out.println("Posicion "+i+": "+arreglo[i]);
+        }
     }
 }
