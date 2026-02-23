f7e3d6c8bee287bb07040dc46d893d783bc0a2cb
XavZol <alansolis707@gmail.com>
2026-02-22 23:10:18 -0600

    Programa que lee 5 números, y los imprime en el mismo orden que fueron introducidos.

--- Vectores.java
+++ Vectores.java
@@ -1,12 +1,24 @@
 
-// Bucle Foreach
+import java.util.Scanner;
+
+
+/*Leer 5 números, guardalos en un arreglo y mostrarlos en el mismo orden introducido. */
 public class Vectores {
 
     public static void main(String[] args) {
-        String[] nombres = {"Alejandro", "Armando", "Luis", "Juan", "Mendo", "Sandía", "Roberto", "Elena"};
+        Scanner entrada = new Scanner(System.in);
+        // Puedes poner los corchetes antes y después de variable
+        float[] numeros = new float[5];
 
-        for(String i:nombres){
-            System.out.println("Nombres: "+i);
-        }
+            System.out.println("Guardando los datos en el arreglo");
+            for(int i=0; i<5; i++) {
+                System.out.print((i+1)+". Digite un número: ");
+                numeros[i] = entrada.nextFloat();
+            }
+
+            System.out.println("\nImprimir los elementos del arreglo");
+            for(float i:numeros){
+                System.out.println(i);
+            }
     }
 }
